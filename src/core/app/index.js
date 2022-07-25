078767e4dd1035f5ee1c7471a144424b995199c0
Thiago Dantas <thiago.dantas@stone.com.br>
2022-07-25 11:53:30 -0300

    feat: generated project and settings

---
+++ src/core/app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => (
+  <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <Text>Hello World</Text>
+  </View>
+);
+
+export default App;
