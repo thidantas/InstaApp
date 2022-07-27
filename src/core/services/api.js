96aae3a2e2cc0b803e82cc6341a700fd3ba75074
Thiago Dantas <104513747+thidantas@users.noreply.github.com>
2022-07-27 17:04:12 -0300

    Revert "feat: setting call at api"

--- src/core/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://dummyapi.io/data/v1/',
-  timeout: 1000,
-  headers: {'app-id': /*id disponibilizado ao cadastar-se no site da API}*/
-});
-
-export default api;
