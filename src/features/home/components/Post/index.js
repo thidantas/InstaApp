0b6835f4f530bbb7f908328006b8c3a5629b03a6
Thiago Dantas <thiago.dantas@stone.com.br>
2022-08-01 16:00:53 -0300

    refactor: refactoring code from the files that make up the project

--- src/features/home/components/Post/index.js
+++ src/features/home/components/Post/index.js
@@ -1,87 +1,71 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
-import {formatDistance} from 'date-fns';
-import {enUS} from 'date-fns/locale';
-import {useNavigation} from '@react-navigation/native';
-import styles from './styles';
 
+import styles from './styles';
 import HeartIcon from '../../assets/icons/heart.svg';
 import HeartRedIcon from '../../assets/icons/heartRed.svg';
 import CommentIcon from '../../assets/icons/comment.svg';
 import BookMarkIcon from '../../assets/icons/bookmark.svg';
 import BookMarkBlackIcon from '../../assets/icons/bookmarkBlack.svg';
+import formatTimePost from '../../utils/formatTimePost';
 
-const Post = ({data}) => {
-  const navigation = useNavigation();
+const Post = ({
+  data,
+  isLiked,
+  isSaved,
+  onLikePost,
+  onSavePost,
+  onNavigateToComments,
+}) => (
+  <View style={styles.container}>
+    <View style={styles.header}>
+      <View style={styles.content}>
+        <Image source={{uri: data.owner.picture}} style={styles.avatar} />
 
-  const [like, setLike] = useState(false);
-  const [save, setSave] = useState(false);
-
-  const handleLikePost = () => {
-    setLike(!like);
-  };
-
-  const handleSavePost = () => {
-    setSave(!save);
-  };
-
-  const formatTimePost = () => {
-    // console.log(data.publishDate);
-    const datePost = new Date(data.publishDate);
-    return formatDistance(new Date(), datePost, {
-      locale: enUS,
-    });
-  };
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <View style={styles.content}>
-          <Image source={{uri: data.owner.picture}} style={styles.avatar} />
-          <TouchableOpacity style={styles.buttonName} onPress={() => {}}>
-            <Text style={styles.name}>{data.owner.firstName}</Text>
-          </TouchableOpacity>
-        </View>
-        <View>
-          <Text style={styles.timePost}>{formatTimePost()}</Text>
-        </View>
+        <TouchableOpacity style={styles.buttonName} onPress={() => {}}>
+          <Text style={styles.name}>{data.owner.firstName}</Text>
+        </TouchableOpacity>
       </View>
       <View>
-        <Image source={{uri: data.image}} style={styles.post} />
-      </View>
-      <View style={styles.icons}>
-        <View style={styles.contentIcons}>
-          <TouchableOpacity onPress={handleLikePost}>
-            {!like ? (
-              <HeartIcon style={styles.like} />
-            ) : (
-              <HeartRedIcon style={styles.comment} />
-            )}
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => alert('abre a página de comentários')} // navigation.navigate('Comments')}
-            style={styles.commentButton}>
-            <CommentIcon style={styles.comment} />
-          </TouchableOpacity>
-        </View>
-        <View>
-          <TouchableOpacity onPress={handleSavePost}>
-            {!save ? (
-              <BookMarkIcon style={styles.saved} />
-            ) : (
-              <BookMarkBlackIcon style={styles.saved} />
-            )}
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View>
-        <Text style={styles.likes}>{data.likes} Likes</Text>
-      </View>
-      <View style={styles.commentPost}>
-        <Text style={styles.userName}>{data.owner.firstName}</Text>
-        <Text style={styles.userComment}>{data.text}</Text>
+        <Text style={styles.timePost}>{formatTimePost(data.publishDate)}</Text>
       </View>
     </View>
-  );
-};
+    <View>
+      <Image source={{uri: data.image}} style={styles.post} />
+    </View>
+    <View style={styles.icons}>
+      <View style={styles.contentIcons}>
+        <TouchableOpacity onPress={onLikePost}>
+          {!isLiked ? (
+            <HeartIcon style={styles.like} />
+          ) : (
+            <HeartRedIcon style={styles.comment} />
+          )}
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={onNavigateToComments}
+          style={styles.commentButton}>
+          <CommentIcon style={styles.comment} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity onPress={onSavePost}>
+          {!isSaved ? (
+            <BookMarkIcon style={styles.saved} />
+          ) : (
+            <BookMarkBlackIcon style={styles.savedimage} />
+          )}
+        </TouchableOpacity>
+      </View>
+    </View>
+    <View>
+      <Text style={styles.likes}>{data.likes} Likes</Text>
+    </View>
+    <View style={styles.commentPost}>
+      <Text style={styles.userName}>{data.owner.firstName}</Text>
+      <Text style={styles.userComment}>{data.text}</Text>
+    </View>
+  </View>
+);
 
 export default Post;
--- src/features/home/screens/Feed/index.js
+++ src/features/home/screens/Feed/index.js
@@ -1,9 +1,12 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import FeedContainer from './ui';
 import postService from '../../../../core/services/posts';
+import routes from '../../../../core/constants/routes';
 
-const Feed = () => {
+const Feed = ({navigation}) => {
   const [posts, setPosts] = useState([]);
+  const [likedPosts, setLikedPosts] = useState([]);
+  const [savedPosts, setSavedPosts] = useState([]);
 
   const fetchPosts = useCallback(async () => {
     const response = await postService.list();
@@ -11,10 +14,35 @@ const Feed = () => {
     setPosts(response.data);
   }, [setPosts]);
 
+  const handleOnLikePost = useCallback(postId => {
+    // Verificar se esse post já tem o seu like
+    // Atualizar a quantidade de likes na api
+    // Atualizar a lista de likedPosts
+  }, []);
+
+  const handleOnSavePost = useCallback(postId => {
+    // Verificar se esse post já está salvo
+    // Atualizar a lista de posts salvos
+  }, []);
+
+  const handleOnNavigateToComments = useCallback(() => {
+    navigation.navigate(routes.Comments.itself);
+  }, [navigation]);
+
   useEffect(() => {
     fetchPosts();
   }, [fetchPosts]);
 
-  return <FeedContainer posts={posts} />;
+  return (
+    <FeedContainer
+      posts={posts}
+      savedPosts={savedPosts}
+      likedPosts={likedPosts}
+      onLikePost={handleOnLikePost}
+      onSavePost={handleOnSavePost}
+      onNavigateToComments={handleOnNavigateToComments}
+    />
+  );
 };
+
 export default Feed;
