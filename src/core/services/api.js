a36f7fd2bdec915f72774a1020aa111f1e993da3
Thiago Dantas <104513747+thidantas@users.noreply.github.com>
2022-08-17 12:02:37 -0300

    Delete api.js

--- src/core/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://dummyapi.io/data/v1/',
-  timeout: 1000,
-  headers: {'app-id': '62cc6e4eb1270cde1dba8918'},
-});
-
-export default api;
